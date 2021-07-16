8fa96671a5efadc27f28382649964e243b8e012b
mahiro <mahiro.yoshie@gmail.com>
2021-07-16 10:20:56 +0900

    mahi_rf.py

--- src/mahi_rf.py
+++ src/mahi_rf.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import sys,os
 from sklearn import preprocessing
+import matplotlib.pyplot as plt
+
 
 sys.path.append(os.path.abspath("."))
 
@@ -48,28 +50,39 @@ def run():
         train.drop('Survived',axis = 1),
         train['Survived'],
         test_size=0.4
-        )
+    )
 
+    
 
-    lr = LogisticRegression()
     rf = RandomForest()
-    xgb = XGBoost()
 
-    model = lr.model
+    model = rf.model
+
+    sfm_columns = rf.sfm(X_train,y_train,test.columns)
+
+    X_train = X_train[sfm_columns]
+    X_val = X_val[sfm_columns]
+
+    test = test[sfm_columns]
+
 
     model.fit(X_train,y_train)
+    
     y_pre = model.predict(X_val)
 
-    print(f'score={lr.score(y_val,y_pre)}')
-
-    y_pre_sub = model.predict(test)
-
+    
+    
+    print(f'score={rf.accuracy_score(y_val,y_pre)}')
 
 
     """ submit """
-    # sub = pd.read_csv(r'input\gender_submission.csv')
-    # sub['Survived'] = y_pre_sub.astype(int)
-    # sub.to_csv(r'sub\submission_rf.csv',index=False)
+    y_pre_sub = model.predict(test)
+    rf.submit(
+        y_predict=y_pre_sub,
+        name='rf_sfm'
+    )
+
+
 
 if __name__ == "__main__":
     run()
